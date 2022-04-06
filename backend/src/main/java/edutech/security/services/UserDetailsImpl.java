f39b959788fb134e69b3572fe0ecf9334c3e4d9d
wiamebouaboub <wiame.bouaboub@etud.univ-jfc.fr>
2022-04-06 15:39:55 +0200

    suppression import dans userDetailsImpl

--- backend/src/main/java/edutech/security/services/UserDetailsImpl.java
+++ backend/src/main/java/edutech/security/services/UserDetailsImpl.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,6 +17,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Service
 public class UserDetailsImpl implements UserDetails {
     private static final long serialVersionUID = 1L;
+    
     private int id;
     private String first_name;
     private String last_name;
@@ -23,6 +25,7 @@ public class UserDetailsImpl implements UserDetails {
     @JsonIgnore
     private String password;
     private Collection<? extends GrantedAuthority> authorities;
+
 
     public UserDetailsImpl(int id, String first_name, String last_name, String email, String password,
             Collection<? extends GrantedAuthority> authorities) {
